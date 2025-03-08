cdc5a4c20db1e2acca83be7881e2db952ed4e0b7
shakeelmahdhy <shakeelahmedmahdhy@gmail.com>
2025-03-08 15:13:44 +1100

    Update CORS origin

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://blog-page-mdy9.onrender.com"],
+    allow_origins=["https://blog-page-lemon-ten.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
